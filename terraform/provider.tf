fe4e9b0cefd3bc19be0ce27a633b092e5bb197e4
Felipe Silva <felipefrs7@gmail.com>
2025-02-08 22:26:44 -0300

    Remove files to configure Terraform

--- terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Configurações de autenticação podem ser feitas via variáveis de ambiente ou perfil.
-# Exemplo de variáveis de ambiente:
-# AWS_ACCESS_KEY_ID=your-access-key-id
-# AWS_SECRET_ACCESS_KEY=your-secret-access-key
