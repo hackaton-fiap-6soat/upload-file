5e3ac3b98b5f2ce668bf22426d3793cab1ce498b
jubel <jubelcassioverridasilva@gmail.com>
2025-02-09 21:26:47 -0300

    Included lambda inside de VPC

--- src/app/controllers/lambda_handler.py
+++ src/app/controllers/lambda_handler.py
@@ -17,11 +17,13 @@ def lambda_handler(event, context):
         content_type = headers.get("content-type")
 
         # Instanciar dependências
+        print("Instanciando dependências")
         s3_adapter = S3Adapter()
         sqs_adapter = SQSAdapter()
-        upload_service = UploadService(s3_adapter, sqs_adapter, max_file_size=100 * 1024 * 1024)
+        upload_service = UploadService(s3_adapter, sqs_adapter, max_file_size=100 * 1024 * 1024, username=username)
 
         # Criar entidade do arquivo e processar o upload
+        print("Gerando URL pré-assinada")
         file = File(name=file_name, size=file_size, content_type=content_type)
         upload_url = upload_service.upload_file(file)
 
